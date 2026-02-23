ae9274776c492d1a5a12a27a1eaf61e731473e09
shyammohanbhat <shyammohanbhat@gmail.com>
2026-02-24 00:43:35 +0530

    ListNode code

---
+++ src/src/com/revision/linkedlist/ListNode.java
@@ -0,0 +1,122 @@
+package com.revision.linkedlist;
+
+import java.util.List;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(){}
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+    ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
+
+    public void printListNode(ListNode head){
+        while(head != null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    /*
+    * Time Complexity: O(1)
+    * */
+    public ListNode insertFirst(ListNode head, ListNode node){
+        if (head == null) {
+            return node;
+        }
+
+        node.next = head;
+        head = node;
+        return head;
+    }
+
+    /*
+    * Time Complexity: O(n)
+     */
+    public ListNode insertLast(ListNode head, ListNode node){
+        if (head == null) {
+            return node;
+        }
+
+        ListNode curr = head;
+
+        while(curr.next != null){
+            curr = curr.next;
+        }
+
+        curr.next = node;
+        return head;
+    }
+
+    /*
+     * Time Complexity: O(n)
+     */
+    public ListNode deleteNode(ListNode head, ListNode node){
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode curr = dummy;
+
+        while (curr.next != null){
+            if(curr.next == node){
+                curr.next = curr.next.next;
+                break;
+            }
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode deleteHeadNode(ListNode head){
+        if(head == null) return  null;
+        head = head.next;
+        return head;
+    }
+
+    /*
+     * Time Complexity: O(n)
+     */
+    public ListNode deleteTailNode(ListNode head){
+        if(head == null) return  null;
+        if(head.next == null) return null;
+
+        ListNode curr = head;
+        while(curr.next.next != null){
+            curr = curr.next;
+        }
+        curr.next = null;
+        return  head;
+    }
+
+    public static void main() {
+        ListNode listNode = new ListNode();
+        ListNode l = new ListNode(10);
+        ListNode node = new ListNode(5);
+        l = listNode.insertFirst(l, node);
+        l = listNode.insertLast(l, new ListNode(11));
+        l = listNode.insertFirst(l, new ListNode(12));
+        l = listNode.insertFirst(l, new ListNode(13));
+        listNode.printListNode(l);
+
+        System.out.println("------------------------------------");
+        l=listNode.deleteHeadNode(l);
+        listNode.printListNode(l);
+
+        System.out.println("------------------------------------");
+
+        l=listNode.deleteNode(l, node);
+        listNode.printListNode(l);
+
+        System.out.println("------------------------------------");
+
+        l=listNode.deleteTailNode(l);
+        listNode.printListNode(l);
+    }
+}
